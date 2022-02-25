6a1eabb97f4d72180b588bce82510b869d4c649b
Michael Clermont <michael.clermont@mths.ca>
2022-02-25 19:47:30 +0000

    edited area_and_perimeter program

--- area_and_perimeter.py
+++ area_and_perimeter.py
@@ -13,8 +13,8 @@ def main():
 
     print("If a circle has a radius of 15 mm: ")
     print("")
-    print("Area is {}mm².".format(math.pi * 15**2))
-    print("Perimeter is {}cm.".format(2 * math.pi * 15))
+    print("Area is {} mm².".format(math.pi * 15**2))
+    print("Perimeter is {} mm.".format(2 * math.pi * 15))
 
 
 if __name__ == "__main__":
